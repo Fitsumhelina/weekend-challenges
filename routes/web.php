ed31491086404fa1a6c002a2ff68fd5a18a17637
fitsumhelina <dev.fitsum@gmail.com>
2025-07-18 11:52:20 +0300

    feat: Refactor LoginRequest class and enhance web routes for expenses and income management

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,24 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/expenses', [ProfileController::class, 'expenses'])->name('expenses');
+    Route::get('/income', [ProfileController::class, 'income'])->name('income');
+    Route::get('/income/{id}', [ProfileController::class, 'showIncome'])->name('income.show');
+    Route::get('/expenses/{id}', [ProfileController::class, 'showExpense'])->
+    Route::middleware(['role:Admin'])->group (function () {
+        Route::post('/expenses', [ProfileController::class, 'storeExpense'])->name('expenses.store');
+        Route::post('/income', [ProfileController::class, 'storeIncome'])->name('income.store');
+        Route::put('/expenses/{id}', [ProfileController::class, 'updateExpenses'])->name('expenses.update');
+        Route::put('/income/{id}', [ProfileController::class, 'updateIncome'])->name('income.update');
+        Route::get('/users', [ProfileController::class, 'users'])->name('users');
+        Route::delete('expenses/{id}', [ProfileController::class, 'deleteExpense'])->name('expenses.delete');
+        Route::delete('income/{id}', [ProfileController::class, 'deleteIncome'])->name('income.delete');
+        Route::get('/reports', [ProfileController::class, 'reports'])->name('reports');
+        // Route::get('/settings', [ProfileController::class, 'settings'])->name('settings');
+ 
+    });
 });
+
+
 
 require __DIR__.'/auth.php';
