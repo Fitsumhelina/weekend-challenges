f5a2b6ee327eb234c276ada5b79ccd1282b596f6
fitsumhelina <dev.fitsum@gmail.com>
2025-07-19 15:32:15 +0300

    refactor: Reorder seeder calls in DatabaseSeeder for clarity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
        $this->call([
+           PermissionSeeder::class,
+           UserSeeder::class,
            ExpenseSeeder::class,
            IncomeSeeder::class,
-           UserSeeder::class,
-           PermissionSeeder::class,
        ]);
 
     }
