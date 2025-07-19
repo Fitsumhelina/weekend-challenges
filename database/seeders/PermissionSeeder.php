1554668349e8c4edeedc69fd770ffa7c7e27ad32
fitsumhelina <dev.fitsum@gmail.com>
2025-07-19 10:49:43 +0300

    feat: Implement PermissionSeeder to define and assign roles and permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Define all permissions
+        $permissions = [
+            // income permissions
+            'create income',
+            'update income',
+            'delete income',
+            'view income',
+
+            // expenses management
+            'create expense',
+            'update expense',
+            'delete expense',
+            'view expense',
+
+            // Role management
+            'create role',
+            'view role',
+            'update role',
+            'delete role',
+
+            // Permission management
+            'create permission',
+            'view permission',
+            'update permission',
+            'delete permission',
+        ];
+
+        // Create or update permissions
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'web']);
+        }
+
+        // Create or update roles
+        $admin   = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        $member  = Role::firstOrCreate(['name' => 'member', 'guard_name' => 'web']);
+
+
+        // Assign permissions
+        $admin->syncPermissions(Permission::all());
+
+        $member->syncPermissions([
+            'view expense',
+            'view income',
+        ]);
+
+   
+    }
+}
