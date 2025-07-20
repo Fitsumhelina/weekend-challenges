65c18acdde0d7d466a4e1e49fba8956f3a6f0303
fitsumhelina <dev.fitsum@gmail.com>
2025-07-20 10:53:37 +0300

    feat: Add role and permission routes with CRUD operations

---
+++ routes/role.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PermissionController;
+
+
+Route::get('roles/', [RoleController::class, 'index'])->name('roles.index');
+Route::get('roles/create', [RoleController::class, 'create'])->name('roles.create');
+Route::post('roles', [RoleController::class, 'store'])->name('roles.store');
+Route::get('roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+Route::match(['PUT', 'PATCH'], '/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+// Route::put('roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+// Route::delete('roles/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
+
+Route::patch('roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
+Route::resource('permissions', PermissionController::class)->except(['create', 'show']);
+Route::get('permissions/{id}/edit', [PermissionController::class, 'edit'])->name('permissions.edit');
+Route::match(['PUT', 'PATCH'], '/permissions/{permission}', [PermissionController::class, 'update'])->name('roles.permissions');
+// Route::put('permissions/{permission}', [PermissionController::class, 'update'])->name('permissions.update');
+
+
