27a197dd1ee26c9fce7f3e3eae282cefbb1b4799
fitsumhelina <dev.fitsum@gmail.com>
2025-07-30 14:32:30 +0300

    feat: Add authentication middleware for Todo routes

--- week-3/routes/web.php
+++ week-3/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
+    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+    Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
+    Route::post('/todos/update-status', [TodoController::class, 'updateStatus'])->name('todos.updateStatus');
 });
