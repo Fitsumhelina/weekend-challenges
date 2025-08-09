5bd5029fd26a7236a1604b6a4878bdc024ecbc4a
fitsumhelina <dev.fitsum@gmail.com>
2025-08-09 11:25:02 +0300

    Refactor PermissionSeeder to correctly assign permissions to roles and reorder permission creation

--- week-5/database/seeders/PermissionSeeder.php
+++ week-5/database/seeders/PermissionSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class PermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+      
+        // Permissions
+        Permission::firstOrCreate(['name' => 'edit-contact-info']);
+        Permission::firstOrCreate(['name' => 'edit-service']);
+
+        // Roles
+        $admin1 = Role::firstOrCreate(['name' => 'admin1']);
+        $admin2 = Role::firstOrCreate(['name' => 'admin2']);
+
+        // Assign permissions to roles
+        $admin1->givePermissionTo('edit-service');
+        $admin2->givePermissionTo('edit-contact-info');
     }
 }
