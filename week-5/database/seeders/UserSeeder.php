3490f9befcd22bc97f0a796ac48e867a5be6531c
fitsumhelina <dev.fitsum@gmail.com>
2025-08-14 16:06:23 +0300

    Update permission names in PermissionSeeder for consistency; change passwords in UserSeeder for security; add empty README files for week-1 and main directory.

--- week-5/database/seeders/UserSeeder.php
+++ week-5/database/seeders/UserSeeder.php
@@ -14,14 +14,14 @@ class UserSeeder extends Seeder
         $user1 = User::create([
             'name' => 'Admin One',
             'email' => 'admin1@admin.com',
-            'password' => bcrypt('password123'),
+            'password' => bcrypt('12345678'),
         ]);
         $user1->assignRole('admin1');
 
         $user2 = User::create([
             'name' => 'Admin Two',
             'email' => 'admin2@admin.com',
-            'password' => bcrypt('password123'),
+            'password' => bcrypt('12345678'),
         ]);
         $user2->assignRole('admin2');
     }
